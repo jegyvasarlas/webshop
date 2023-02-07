036cc76260fda25a5a951cb1158a7cab2a21a8c3
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2023-02-07 11:49:05 +0100

    Initial commit

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 include 'files/php/main.php'
 ?>
-<!DOCTYPE
+<!DOCTYPE html>
+<html>
+<head>
+<title>Webaruhaz</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="files/css/bootstrap.min.css">
+<link rel="stylesheet" href="files/css/style.css">
+<script src="files/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+</body>
+</html>
+
